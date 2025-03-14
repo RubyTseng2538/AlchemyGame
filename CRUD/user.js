899f69e59fb481b6d4c46ee1732c550719a97454
RubyTseng2538 <86134957+RubyTseng2538@users.noreply.github.com>
2025-03-14 14:27:14 -0700

    Schema created

---
+++ CRUD/user.js
@@ -0,0 +1,121 @@
+const {Prisma, PrismaClient} = require('@prisma/client');
+const prisma = new PrismaClient();
+
+
+//create
+async function createUser(data) {
+    const user = await prisma.user.create({
+      data: data
+    });
+    return user;
+}
+
+//read
+async function getUserById(id){
+    const user = await prisma.user.findUnique({
+        where:{
+            id: id
+           }
+    });
+    return user
+}
+
+async function getUserByGoogleId(id){
+    const user = await prisma.user.findUnique({
+        where:{
+            GoogleId: id
+           }
+    });
+    return user
+}
+
+async function getUserByEmail(email){
+    const user = await prisma.user.findUnique({
+        where:{
+            email: email
+        }
+    });
+    return user;
+}
+
+
+//combine email and name contain
+async function getUserByString(string){
+    const users = await prisma.user.findMany({
+        where:
+            {OR: [{
+                email: {
+                    contains: string,
+                    mode: 'insensitive'
+                }
+            },{
+                name: {
+                    contains: string,
+                    mode: 'insensitive'
+                }
+            }
+        ]}
+    });
+    return users
+}
+
+async function getAllUsers(){
+    const users = await prisma.user.findMany();
+    return users;
+}
+
+
+//update
+async function updateUser(id, data){
+    const user = await prisma.user.update({
+        where:{
+            id: id
+        },
+        data: data
+    });
+    return user;
+}
+
+async function addEvents(id, eventID){
+    const event = await prisma.user.update({
+        where: {
+            id: id
+        },
+        data: {
+            events: {connect: {id: eventID}}
+        }
+    })
+}
+
+async function removeEvents(eventID, userID){
+    const user = await prisma.user.update({
+        where: {
+            id: userID
+        },
+        data: {
+            events: {disconnect: {id: eventID}}
+        }
+    })
+}
+
+//delete
+async function deleteUser(id){
+    const user = await prisma.user.delete({
+        where:{
+            id: id
+        }
+    })
+}
+
+module.exports = {
+  createUser,
+  getUserById,
+  getUserByGoogleId,
+  getUserByEmail,
+  getUserByString,
+  getAllUsers,
+  updateUser,
+  addEvents,
+  removeEvents,
+  deleteUser
+};
